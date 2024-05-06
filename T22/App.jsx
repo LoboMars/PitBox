6c93726efebe5c9882cada1a07c21a17783ff417
Scorp10nZ <adriano.pinto.2004@gmail.com>
2024-05-06 16:54:12 +0100

    criação do Openning Screen

--- T22/App.jsx
+++ T22/App.jsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import OScreen from './View/OScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <OScreen/>
       <StatusBar style="auto" />
     </View>
   );
