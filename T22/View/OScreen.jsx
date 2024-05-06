7b320707fb598da763e32ed7e89f81faebc08e1b
Scorp10nZ <adriano.pinto.2004@gmail.com>
2024-05-06 17:39:00 +0100

    criar botões login/Register

--- T22/View/OScreen.jsx
+++ T22/View/OScreen.jsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Dimensions, TouchableOpacity } from 'react-native';
 
 export default function OScreen() {
     return (
@@ -7,7 +7,10 @@ export default function OScreen() {
             <Text style={styles.textWhite}>Conserto <Text style={styles.textPurple}>rápido</Text>, serviço de <Text style={styles.textPurple}>qualidade</Text>.</Text>
             <Text style={styles.textWhite}>A nossa App é a chave</Text>
             <View>
-            <Text style={styles.textWhite}>Login</Text>
+            <Text style={styles.Login}>Login</Text>
+            </View>
+            <View>
+            <Text style={styles. Register}>Register</Text>
             </View>
           </View>
       );
@@ -28,8 +31,22 @@ const styles = StyleSheet.create({
   textPurple: {
     color:'#6D4EE5'
   },
+  Register: {
+    backgroundColor: '#6D4EE5',
+    color: 'white',
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 5,
+    marginTop: 20,
+  },
   Login: {
-    
-  }
+    marginTop: 20,
+    color: 'white',
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 5,
+    paddingVertical: 15,
+    paddingHorizontal: 45,
+  },
 
 });
