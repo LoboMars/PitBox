6054f21884d49d45c67742b6a5898e339e3fc51d
Scorp10nZ <adriano.pinto.2004@gmail.com>
2024-05-06 17:53:54 +0100

    +atualização

--- T22/View/OScreen.jsx
+++ T22/View/OScreen.jsx
@@ -3,18 +3,16 @@ import { StyleSheet, Text, View , Dimensions, TouchableOpacity } from 'react-nat
 
 export default function OScreen() {
     return (
-          <View style={styles.container}>
-            <Text style={styles.textWhite}>Conserto <Text style={styles.textPurple}>rápido</Text>, serviço de <Text style={styles.textPurple}>qualidade</Text>.</Text>
-            <Text style={styles.textWhite}>A nossa App é a chave</Text>
-            <View>
-            <Text style={styles.Login}>Login</Text>
-            </View>
-            <View>
-            <Text style={styles. Register}>Register</Text>
-            </View>
-          </View>
-      );
-}
+      <View style={styles.container}>
+        <Text style={styles.textWhite}>Conserto <Text style={styles.textPurple}>rápido</Text>, serviço de <Text style={styles.textPurple}>qualidade</Text>.</Text>
+        <Text style={styles.textWhite}>A nossa App é a chave</Text>
+        <View>
+          <Text style={styles.Login}>Login</Text>
+          <Text style={styles.Register}>Register</Text>
+        </View>
+      </View>
+    );
+  }
 
 const styles = StyleSheet.create({
   container: {
@@ -23,7 +21,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#232427',
     alignItems: 'center',
     justifyContent: 'flex-start',
-    paddingTop: '80%',
+    paddingTop: '60%',
   },
   textWhite: {
     color: 'white',
@@ -31,22 +29,29 @@ const styles = StyleSheet.create({
   textPurple: {
     color:'#6D4EE5'
   },
-  Register: {
-    backgroundColor: '#6D4EE5',
-    color: 'white',
-    paddingVertical: 15,
-    paddingHorizontal: 40,
-    borderRadius: 5,
-    marginTop: 20,
-  },
   Login: {
-    marginTop: 20,
+    marginTop: '150%',
     color: 'white',
     borderWidth: 1,
     borderColor: 'white',
     borderRadius: 5,
-    paddingVertical: 15,
-    paddingHorizontal: 45,
+    paddingVertical: 25,
+    paddingHorizontal: 75,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
+  Register: {
+    marginTop: '15%',
+    backgroundColor: '#6D4EE5',
+    color: 'white',
+    paddingVertical: 25,
+    paddingHorizontal: 70,
+    borderRadius: 5,
+    paddingHorizontal: 75,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
 
 });
