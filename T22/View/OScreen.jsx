6ae0efc31480e109729fbd56ba2bba674a65aaa1
Scorp10nZ <adriano.pinto.2004@gmail.com>
2024-05-06 17:31:21 +0100

    alterações OScreen

--- T22/View/OScreen.jsx
+++ T22/View/OScreen.jsx
@@ -2,20 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function OScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>PitBox</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+          <View style={styles.container}>
+            <Text style={styles.textWhite}>Conserto <Text style={styles.textPurple}>rápido</Text>, serviço de <Text style={styles.textPurple}>qualidade</Text>.</Text>
+            <Text style={styles.textWhite}>A nossa App é a chave</Text>
+            <View>
+            <Text style={styles.textWhite}>Login</Text>
+            </View>
+          </View>
+      );
 }
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%' ,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#232427',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: '80%',
   },
-});
+  textWhite: {
+    color: 'white',
+  },
+  textPurple: {
+    color:'#6D4EE5'
+  },
+  Login: {
+    
+  }
 
+});
