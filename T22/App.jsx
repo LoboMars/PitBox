867eb93aca13bfb303ae4dcc96031986eb355b1e
Scorp10nZ <adriano.pinto.2004@gmail.com>
2024-05-11 01:14:24 +0100

    RegistarQuotidiano feito Registar Assitencia feito Registar Viatura em processo

--- T22/App.jsx
+++ T22/App.jsx
@@ -3,11 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import OScreen from './View/OScreen';
 import Login from './View/Login';
 import Register from './View/Register';
+import  RegistarQuotidino from './View/RegistarQuotidiano'
+import RegistarAssistencia from './View/RegistarAssistencia';
+import RegistarViatura from './View/RegistarViatura';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Register/>
+        <RegistarViatura/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ T22/View/RegistarViatura.jsx
@@ -0,0 +1,206 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+
+export default function RegistarViatura() {
+  return (
+   
+    <View style={styles.container}>
+    <ScrollView  contentContainerStyle={styles.scrollView}> 
+      <Text style={styles.BigText}>Registar Viatura</Text>
+      
+      <View style={{ marginTop: "5%" }}/>
+      <View style={styles.line} />
+
+      <View style={styles.textBoxContainer}>
+        <Text style={styles.smallText}>Viatura:</Text>
+      </View>
+
+      <View style={styles.textBoxContainer}>
+        <Text style={styles.TextBox}>Local Partida</Text>
+      </View>
+
+      <View style={{ marginTop: "5%" }} />
+
+      <View style={styles.textBoxContainer}>
+        <Text style={styles.smallText}>Oficina:</Text>
+      </View>
+
+      <View style={styles.textBoxContainer}>
+        <Text style={styles.TextBox}>Escolher</Text>
+      </View>
+
+      <View style={{ marginTop: "5%" }} />
+
+      <View style={styles.textBoxContainer}>
+        <Text style={styles.smallText}>Data da Assistência:</Text>
+      </View>
+
+      <View style={styles.textBoxContainer}>
+        <Text style={styles.TextBox}>DD/MM/YYYY</Text>
+      </View>
+
+      <View style={{ marginTop: "5%" }} />
+
+      <View style={styles.textBoxContainer}>
+        <View style={{flexDirection:'row'}}>
+            <View style={{ marginTop: "10%" }} >
+                <Text style={styles.smallText}>Valor da Fatura:</Text>
+            </View>
+          <View style={{marginHorizontal: '1%'}} />
+          <Text style={styles.SmallTextBox}>00.00$:</Text>
+        </View>
+      </View>
+
+
+      <View style={{ marginTop: "5%" }} />
+
+      <View style={styles.textBoxContainer}>
+        <Text style={styles.smallText}>Detalhes:</Text>
+      </View>
+
+      <View style={styles.textBoxContainer}>
+      <Text style={styles.TextBox}>Tipo de Assistencia</Text>
+        <Text style={styles.BigTextBox}>Procedimentos  Aplicados</Text>
+      </View>
+      
+
+      <View style={{ marginTop: "10%" }} />
+      <View style={styles.line} />
+      <View style={{ marginTop: "5%" }} />
+
+      <TouchableOpacity>
+        <View style={styles.textBoxContainer}>
+          <Text style={styles.BTNpurple}>Registar</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <View style={styles.textBoxContainer}>
+          <Text style={styles.Cancelar}>Cancelar</Text>
+        </View>
+      </TouchableOpacity>
+
+      <View style={{ marginTop: "5%" }} />
+
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    paddingTop: '10%', 
+  },
+  container: {
+    width: "100%",
+    flex: 1,
+    backgroundColor: "#232427",
+    textAlign: "center",
+    justifyContent: "center",
+  },
+  textBoxContainer: {
+    width: "85%",
+    alignItems: "left",
+    marginLeft: "8%",
+  },
+  textWhite: {
+    color: "white",
+  },
+  textPurple: {
+    color: "#6D4EE5",
+  },
+  line: {
+    backgroundColor: "#6D4EE5",
+    height: 3,
+    width: "100%",
+    marginBottom: "5%",
+  },
+
+  BigText: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+
+  smallText: {
+    color: "white",
+    fontSize: 15,
+    fontWeight: "bold",
+    textAlign: "left",
+    marginLeft: "8%",
+  },
+
+  TextBox: {
+    width: "100%",
+    color: "white",
+    backgroundColor: "#383343",
+    borderWidth: 1,
+    borderColor: "white",
+    marginTop: "5%",
+    borderRadius: 15,
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    fontSize: 18,
+    textAlign: "left",
+  },
+
+  SmallTextBox: {
+    width: "45%",
+    color: "white",
+    backgroundColor: "#383343",
+    borderWidth: 1,
+    borderColor: "white",
+    marginTop: "5%",
+    borderRadius: 15,
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    fontSize: 15,
+    textAlign: "center",
+  },
+
+  BigTextBox: {
+    width: "100%",
+    color: "white",
+    backgroundColor: "#383343",
+    borderWidth: 1,
+    borderColor: "white",
+    marginTop: "5%",
+    borderRadius: 15,
+    paddingVertical: 100,
+    paddingHorizontal: 40,
+    fontSize: 18,
+    textAlign: "center",
+  },
+
+  BTNpurple: {
+    backgroundColor: "#6D4EE5",
+    color: "white",
+    borderRadius: 15,
+    paddingVertical: 25,
+    paddingHorizontal: 55,
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  Cancelar: {
+    marginTop: "5%",
+    color: "#6D4EE5",
+    borderWidth: 1,
+    borderColor: "#6D4EE5",
+    borderRadius: 15,
+    paddingVertical: 25,
+    paddingHorizontal: 75,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
