80777cf49f703e25404a118f78938b8b3b384ee7
Bruno Lobo <a54916@alunos.ipb.pt>
2024-05-13 16:17:13 +0100

    Update Visual na Main Page

--- T22/View/MainPage.jsx
+++ T22/View/MainPage.jsx
@@ -27,138 +27,150 @@ export default function MainPage() {
 
   return (
     <View style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollView}>
 
-        <View style={styles.container1}> 
-        <View style={{ marginTop: "5%" }} />
-          <View style={styles.imageContainer}>
-            <TouchableOpacity onPress={handleLogout} style={{paddingLeft:'5%'}}>
-              <Image source={LogOut} style={styles.logo} />
+
+      <View style={styles.container1}>
+
+
+        <View style={styles.imageContainer}>
+          <TouchableOpacity onPress={handleLogout} style={{ paddingLeft: '2%' }}>
+            <View>
+              <Image source={LogOut} style={styles.logo}/>
+              <Text style={{color: "#C33434", fontWeight: "700"}}>LogOut</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handleLogout}>
+            <View>
+              <Image source={EditarPerfil} style={styles.logo}/>
+              <Text style={{color: "#9F9BA8", fontWeight: "700"}}>Editar Perfil</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+
+      <View style={styles.container2}>
+
+        <View>
+          <Text style={styles.BigText}>Bem Vindo,</Text>
+        </View>
+        <View>
+          <Text style={styles.BigTextP}>Lebron<Text style={styles.BigTextW}>.</Text></Text>
+        </View>
+
+        <View style={{ paddingTop: "10%" }} />
+
+        <View style={styles.textBoxContainer}>
+          <ScrollView>
+            <View style={styles.TextBox}>
+              <Text style={styles.BigTextLeft}>Nova Viatura</Text>
+            </View>
+
+
+            <View style={styles.TextBox}>
+              <Text style={styles.BigTextLeft}>Nova Assistência</Text>
+            </View>
+
+            <View style={styles.TextBox}>
+              <Text style={styles.BigTextLeft}>Novo Evento</Text>
+            </View>
+
+          </ScrollView>
+        </View>
+
+      </View>
+
+        <View style={styles.container3}>
+
+          <View style={styles.imageContainer2}>
+            <TouchableOpacity onPress={handleLogout}>
+              <View>
+                <Image source={Home} style={styles.logo2}/>
+                <Text style={{color: "#6D4EE5", fontWeight: "500"}}>Home</Text>
+              </View>
             </TouchableOpacity>
             <TouchableOpacity onPress={handleLogout}>
-              <Image source={EditarPerfil} style={styles.logo2} />
+              <View>
+                <Image source={Viaturas} style={styles.logo2} />
+                <Text style={{color: "#9F9BA8", fontWeight: "500"}}>Viaturas</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleLogout}>
+              <View>
+                <Image source={Assistencias} style={styles.logo2} />
+                <Text style={{color: "#9F9BA8", fontWeight: "500", textAlign: 'center'}}>Assistências</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleLogout}>
+              <View>
+                <Image source={Eventos} style={styles.logo2} />
+                <Text style={{color: "#9F9BA8", fontWeight: "500"}}>Eventos</Text>
+              </View>
             </TouchableOpacity>
           </View>
-
-          <View style={{ paddingTop: "10%" }} />
-
-          <View>
-            <Text style={styles.BigText}>Bem Vindo,</Text>
-          </View>
-          <View>
-            <Text style={styles.BigTextP}>Lebron</Text>
-          </View>
-
-          <View style={{ paddingTop: "33%" }} />
-
-        </View>
-        
-    
-        <View style={styles.container2}>
-
-          <View style={styles.textBoxContainer}>
-
-           <View style={styles.TextBox}>
-           <Text style={styles.BigTextLeft}>Nova Viatura</Text>
-           </View>
-
-
-           <View style={styles.TextBox}>
-           <Text style={styles.BigTextLeft}>Nova Assistência</Text>
-           </View>
-
-           <View style={styles.TextBox}>
-           <Text style={styles.BigTextLeft}>Novo Evento</Text>
-           </View>
-
-           <View style={{ paddingTop: "10%" }} />
-
-          </View>
         </View>
 
-        <View style={styles.container3}> 
-          <View style={{ marginTop: "5%" }} />
-            <View style={styles.imageContainer2}>
-              <TouchableOpacity onPress={handleLogout} style={{paddingLeft:'5%'}}>
-                <Image source={Home} style={styles.logo2} />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={handleLogout}>
-                <Image source={Viaturas} style={styles.logo2} />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={handleLogout} style={{paddingLeft:'5%'}}>
-                <Image source={Assistencias} style={styles.logo2} />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={handleLogout}>
-                <Image source={Eventos} style={styles.logo2} />
-              </TouchableOpacity>
-            </View>
-            <View style={{ paddingTop: "5%" }} />
-        </View>
-
-      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: "#2D2A2F",
-    paddingTop: "5%",
-  },
 
   container: {
     width: "100%",
     height:'100%',
     flex: 1,
+    backgroundColor: "#232427",
   },
 
   container1: {
-    width: "100%",
-    flex: 5,
-    backgroundColor: "#2D2A2F",
+    marginTop: "5%",
+    flex: 1,
+    justifyContent: 'center',
   },
 
   container2: {
-    width: "100%",
-    flex: 7,
+    flex: 8,
+    marginTop: "5%",
+    marginBottom: "5%",
     backgroundColor: "#232427",
   },
 
   container3: {
-    width: "100%",
-    flex: 2,
+    flex: 1,
     backgroundColor: "#1C1D21",
+    justifyContent: 'center',
   },
 
   textBoxContainer: {
-    width: "85%",
+    width: "95%",
     alignItems: "left",
-    marginLeft: "8%",
+    marginLeft: "3%",
     backgroundColor: "#232427",
   },
 
   imageContainer: {
     flexDirection: "row",
     justifyContent: "space-between", 
-    paddingHorizontal: "10%",
-    width: "100%",
+    paddingHorizontal: "6%",
   },
 
   imageContainer2: {
     flexDirection: "row",
     justifyContent: "space-between", 
-    paddingHorizontal: "2%",
-    width: "100%",
+    paddingHorizontal: "5%",
   },
 
   logo: {
-    width: 50,
-    height: 60,
+    alignSelf: 'center',
+    width: 40,
+    height: 40,
   },
 
   logo2: {
-    width: 65,
-    height: 65,
+    alignSelf: 'center',
+    width: 32,
+    height: 32,
   },
 
 
@@ -182,7 +194,12 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
   },
-
+  BigTextW: {
+    color: "white",
+    fontSize: 35,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
   TextBox: {
     color: "#9F9BA8",
     backgroundColor: "#383343",
